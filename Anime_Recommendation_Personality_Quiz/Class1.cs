aebb7bbf93d2bfc23e0b6653579e105fba89ca8e
Jacquelyn Finucan <jacquelynfinucan@gmail.com>
2022-08-13 17:21:15 -0400

    project created-first push

---
+++ Anime_Recommendation_Personality_Quiz/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anime_Recommendation_Personality_Quiz
+{
+    internal class Class1
+    {
+
+
+
+    }
+}
---
+++ Anime_Recommendation_Personality_Quiz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Anime_Recommendation_Personality_Quiz
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear(); //required to make entire background the color
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Hello, anime fans!");
+
+            Thread.Sleep(3000); //delays the script for 3 seconds in btwn so they don't appear all at once
+
+            Environment.Exit(0); //exits the console application
+
+            //And we're just getting started!
+
+
+
+
+
+
+
+        }
+    }
+}
